8165e87c705c312da2ee88518aabd0f828f41f9b
Ashvini bhatnagar <45563438+AshviniDataAnalysis@users.noreply.github.com>
2018-12-03 18:56:44 +0530

    This project is complete.

--- LinearSearch/src/com/logic/search/linearSearch.java
+++ LinearSearch/src/com/logic/search/linearSearch.java
@@ -1,9 +1,30 @@
 package com.logic.search;
-
+import java.util.*;
 public class linearSearch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner ob = new Scanner(System.in);
+        int a[]= new int[10];
+        int find,n;
+            System.out.println("Enter the array size");
+            n= ob.nextInt();
+            System.out.println("Enter the array element");
+            for(int i=0; i<n; i++)
+            {
+                a[i]= ob.nextInt();
+            }
+            System.out.println("Enter element to be found");
+            find=ob.nextInt();
+        for(int j=0; j<n; j++)
+        {
+            if(find==a[j])
+            {
+                System.out.println(find+": found at:--"+(j+1));
+            }
+            
+        }
 
 	}
 
